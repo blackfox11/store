0deee3e9aa879787658a7436ee17fa9625947979
blackfox11 <nurmity@gmail.com>
2023-02-14 14:57:23 +0300

    Реализовать поиск по ISBN и автору

---
+++ domain/Store/BookService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store
+{
+    public class BookService
+    {
+        private readonly IBookRepository bookRepository;
+        public BookService(IBookRepository bookRepository)
+        {
+            this.bookRepository = bookRepository;
+        }
+        public Book[] GetAllByQuery(string query)
+        {
+            if (Book.IsIsbn(query))
+            {
+                return bookRepository.GetAllByIsbn(query);
+            }
+            else
+            {
+                return bookRepository.GetAllByTitleOrAuthor(query);
+            }
+        }
+    }
+}
