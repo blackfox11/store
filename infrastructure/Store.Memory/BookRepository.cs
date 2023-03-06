dae2baeb4bee436fb5978c0b60581d818bba9d57
blackfox11 <nurmity@gmail.com>
2023-03-06 11:56:56 +0300

    Добавить отображение содержимого корзины

--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Store;
 
@@ -25,6 +26,14 @@ namespace Store.Memory
         public Book GetById(int id)
         {
             return books.Single(book => book.Id == id);
+        }
+
+        public Book[] GetAllByIds(IEnumerable<int> bookIds)
+        {
+            var foundBooks = from book in books
+                             join bookId in bookIds on book.Id equals bookId
+                             select book;
+            return foundBooks.ToArray();
         }
     }
 }
